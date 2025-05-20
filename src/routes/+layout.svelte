42e64fb762585d6439f488a537c3da262e154a13
adair <adairaxe31@hotmail.com>
2025-05-19 21:22:12 -0500

    feat: create proyect

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/melanie">Melanie</a>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+    <h2>
+        Bienvenido , aquí podras realizar tus reportes usando mapas de distintos
+        paises
+    </h2>
+</main>
