9dc3caa44ab9f6086abe2b97f820fe01c3983f55
Adair Abrigo <adair.abrigo@tia.com.ec>
2025-05-26 10:30:48 -0500

    feat: add bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<script>
+<script lang="ts">
+    let countries: Array<string> = ["Ecuador", "Italia"];
+    let countrieSelect = countries[0];
 </script>
 
 <main>
@@ -6,4 +8,13 @@
         Bienvenido , aquí podras realizar tus reportes usando mapas de distintos
         paises
     </h2>
+    <select class="form-select" bind:value={countrieSelect}>
+        {#each countries as value}
+            <option {value}> {value} </option>
+        {/each}
+    </select>
+    <div class="container-map"></div>
+    <div class="container-charge-csv">
+        <h3>Cargue su csv</h3>
+    </div>
 </main>
