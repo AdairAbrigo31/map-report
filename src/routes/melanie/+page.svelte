e2011949b086f014797c2d26a1bbe772a754d1df
adair <adairaxe31@hotmail.com>
2025-07-11 00:14:39 -0500

    change phrase

--- src/routes/melanie/+page.svelte
+++ src/routes/melanie/+page.svelte
@@ -1,14 +1,15 @@
 <main>
     <p class="phrase">
-        Hola Melanie, si dije algo tonto o si te lastime disculpame. Sabes que
-        soy un idiota, quisiera no serlo. Por otro lado debo confesarte que
-        alejarnos ha sido duro aunque creo que ha sido lo mejor. Te pienso a
-        diario, es estupido lo sé pero no puedo evitarlo. Volvernos a
-        encontrarnos fue maravilloso, ya algunos años que mi corazón no se
-        aceleraba tanto sólo por una caricia en el cabello. Espero que consigas
-        la pasantia y que Ludovica te llene de amor, que tu viaje a Dubai se
-        concrete y que las cosas con Jacomo vayan mejor. Mereces a alguien que
-        te cuide y te ame. Cuidate niña bonita.
+        Hola fea, espero no haberte lastimado la ultima vez que hablamos, si lo
+        hice disculpame, a veces sólo digo locuras aunque creo que tú me
+        entenderas ya que tambien a veces lo haces. Queria agreadecerte por todo
+        el amor que me diste, nuestro tiempo juntos son de los recuerdos más
+        bonitos que tengo, incluso los raros como levantar un mueble rumbo a la
+        playa. Y creo que hablo por los dos al decir que sin querer queriendo
+        nos volvimos a conocer y nos volvimos a enamorar. Pero sé que esto no es
+        una pelicula. Fue bonito haber coincidido en este mundo y haber
+        compartido un pedacito de nuestras vidas. Saludame a Ludovica. Un abrazo
+        niña bonita.
     </p>
     <img src="/melanie_adair.jpeg" alt="Melanie" class="melanie-adair-photo" />
 </main>
