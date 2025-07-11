999f5e097b7e802cfe732bcdcace6b5c26f8ca82
adair <adairaxe31@hotmail.com>
2025-07-11 00:06:25 -0500

    change hidden phrase

--- src/routes/melanie/+page.svelte
+++ src/routes/melanie/+page.svelte
@@ -1,7 +1,14 @@
 <main>
     <p class="phrase">
-        Me gustaste por quien eres; y quien eres es alguien que se va. Pero para
-        él, eres alguien que se queda.
+        Hola Melanie, si dije algo tonto o si te lastime disculpame. Sabes que
+        soy un idiota, quisiera no serlo. Por otro lado debo confesarte que
+        alejarnos ha sido duro aunque creo que ha sido lo mejor. Te pienso a
+        diario, es estupido lo sé pero no puedo evitarlo. Volvernos a
+        encontrarnos fue maravilloso, ya algunos años que mi corazón no se
+        aceleraba tanto sólo por una caricia en el cabello. Espero que consigas
+        la pasantia y que Ludovica te llene de amor, que tu viaje a Dubai se
+        concrete y que las cosas con Jacomo vayan mejor. Mereces a alguien que
+        te cuide y te ame. Cuidate niña bonita.
     </p>
     <img src="/melanie_adair.jpeg" alt="Melanie" class="melanie-adair-photo" />
 </main>
