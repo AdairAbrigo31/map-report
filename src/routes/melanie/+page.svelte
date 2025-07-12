e13dd6157d7a95bd4e33d770392774c52b9841b2
adair <adairaxe31@hotmail.com>
2025-07-11 21:57:51 -0500

    change hidden phrase

--- src/routes/melanie/+page.svelte
+++ src/routes/melanie/+page.svelte
@@ -1,15 +1,27 @@
 <main>
     <p class="phrase">
-        Hola fea, espero no haberte lastimado la ultima vez que hablamos, si lo
-        hice disculpame, a veces sólo digo locuras aunque creo que tú me
-        entenderas ya que tambien a veces lo haces. Queria agreadecerte por todo
-        el amor que me diste, nuestro tiempo juntos son de los recuerdos más
-        bonitos que tengo, incluso los raros como levantar un mueble rumbo a la
-        playa. Y creo que hablo por los dos al decir que sin querer queriendo
-        nos volvimos a conocer y nos volvimos a enamorar. Pero sé que esto no es
-        una pelicula. Fue bonito haber coincidido en este mundo y haber
-        compartido un pedacito de nuestras vidas. Saludame a Ludovica. Un abrazo
-        niña bonita.
+        Hola fea, estoy algo mareado, creo que aqui pondre todas las cosas que
+        quisiera decirte y no me atrevo, o más bien, no debo. Es probable que
+        muchas de estas no las leas. Creo que nuestra ultima conversación no
+        salio bien, creo que nos rompimos más. Pero al menos fuimos honestos. A
+        veces pienso en nosotros, estos dias lo he hecho seguido. Pienso en
+        nuestra ultima noche juntos y en la primera. Pienso en que hubiese
+        pasado si yo no me hubiese ido. Creo que la conclusión a la que he
+        llegado es que de todas formas lo hubiera arruinado, era un niño. No
+        sabia lo que tenia. Creó que eso paso por algo, debimos dejarnos para
+        encontrarnos. Hemos alcanzado cosas grandes, estoy orgulloso de eso, de
+        tí. No me place saber que no estamos juntos pero es suficiente para mí
+        saber que estás bien, que él te cuida y que te hace feliz. Mereces ser
+        feliz. Eres una de las personas más buenas que conozco, te preocupas por
+        todo y por todos. Mi niña bonita, siempre te llevo conmigo aunque no
+        estés presente. Espero algún día amar a alguien como te amo a tí. No lo
+        digo con intenciones de sonar grosero o querer olvidarte, lo digo para
+        que tengas una idea de cuanto te quiero. Gracias por buscarme, me
+        hiciste volver a acelerar el corazón con tan sólo acariciar mi cabello.
+        No creo que haya otra vida pero si la hay no sé cómo ni cuando pero te
+        buscaré. Y si acaso en esta vida tú decides regresar y empezar de nuevo,
+        me encantaria hacerlo contigo. Suerte bonita, espero que vayas a Dubai y
+        que Ludovica disfrute de tu ciudado. Eres increible.
     </p>
     <img src="/melanie_adair.jpeg" alt="Melanie" class="melanie-adair-photo" />
 </main>
